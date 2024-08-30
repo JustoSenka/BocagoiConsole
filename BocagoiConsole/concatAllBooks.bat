98fa1a460fe8d40131805260984557dd18d9de2a
Justas Glodenis <glodjus@gmail.com>
2024-08-30 22:52:17 +0300

    Generating all books joined file after build

---
+++ BocagoiConsole/concatAllBooks.bat
@@ -0,0 +1,15 @@
+cat Resources\Book_1.txt > Resources\BooksAll.txt
+echo. >> Resources\BooksAll.txt
+cat Resources\Book_2.txt >> Resources\BooksAll.txt
+echo. >> Resources\BooksAll.txt
+cat Resources\Book_3.txt >> Resources\BooksAll.txt
+echo. >> Resources\BooksAll.txt
+cat Resources\Book_4.txt >> Resources\BooksAll.txt
+echo. >> Resources\BooksAll.txt
+cat Resources\Book_5.txt >> Resources\BooksAll.txt
+echo. >> Resources\BooksAll.txt
+cat Resources\Book_6.txt >> Resources\BooksAll.txt
+echo. >> Resources\BooksAll.txt
+cat Resources\Book_7.txt >> Resources\BooksAll.txt
+echo. >> Resources\BooksAll.txt
+cat Resources\Book_8.txt >> Resources\BooksAll.txt
